b15867a1971d142c514e8f6123779933a9a106f2
nalawadepiyush <nalawadepiyush@gmail.com>
2019-11-10 09:21:27 +0530

    feat: added time to check the time diference in response of redis api and db call api

--- insta_backend/resources/feed/feed.py
+++ insta_backend/resources/feed/feed.py
@@ -1,9 +1,8 @@
 import json
-
 from flask import Blueprint, request, jsonify
-
-from insta_backend.extensions import redis_client
-from insta_backend.models.post.post import PostMethods
+from datetime import datetime
+from insta_backend.extensions import redis_client, db
+from insta_backend.models.post.post import PostMethods, Post
 from insta_backend.models.user.friendship import FollowerMethods
 from insta_backend.models.user.user import UserMethods
 from insta_backend.utils import generate_user_from_auth_token
@@ -16,12 +15,14 @@ bp_feed = Blueprint("feed", __name__)
 def custom_feed():
     auth_token = request.headers.get('access_token')
     current_user = generate_user_from_auth_token(auth_token)
-    #  HIT REDIS FOR ALL POSTS RELATED TO
+    #  HIT REDIS FOR ALL POSTS RELATED T
     followees = FollowerMethods.get_followees(current_user.id)
     posts = []
     for followee in followees:
         user = UserMethods.get_record_with_id(followee.followee_id)
+        start_time = datetime.utcnow()
         user_posts = redis_client.lrange(user.username, 0, -1)
+        print(datetime.utcnow() - start_time)
         for post in user_posts:
             posts.append(json.loads(post))
     return jsonify({"posts": posts})
@@ -37,7 +38,10 @@ def feed():
     posts = []
     for followee in followees:
         user = UserMethods.get_record_with_id(followee.followee_id)
-        for post in user.posts:
+        start_time = datetime.utcnow()
+        user_posts = db.query(Post).filter(Post.user_id == user.id).all()
+        print(datetime.utcnow() - start_time)
+        for post in user_posts:
             posts.append(
                 dict(
                     caption=post.caption,
